aff9a22bb9d1e421f07de645dd83239444de7f31
Tue Nguyen <tnguyen187@myseneca.ca>
2021-12-30 12:54:18 -0500

    Destructured ExpenseForm

---
+++ src/features/expenseForm/BillForm.js
@@ -0,0 +1,78 @@
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  TextField,
+  Button
+} from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+import RemoveIcon from '@mui/icons-material/Remove'
+import React from 'react'
+
+const BillForm = ({ isBillForm, formik, handleDescChange, handlePriceChange, editBillForm }) => {
+  if (isBillForm) {
+    return (
+      <div className="billForm">
+        <TableContainer>
+          <Table sx={{ maxWidth: '60vw' }}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Description</TableCell>
+                <TableCell>Price</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {formik.values.billDesc.map((elem, index) => (
+                <TableRow>
+                  <TableCell>
+                    <TextField
+                      value={formik.values.billDesc[index]}
+                      multiline
+                      onChange={(e) => handleDescChange(e, index)}
+                      sx={{ width: '40vw' }}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <TextField
+                      sx={{ width: '200px ' }}
+                      value={formik.values.billPrice[index]}
+                      onChange={(e) => handlePriceChange(e, index)}
+                      type="number"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Button onClick={(e) => editBillForm(e, 'add')}>
+                      <AddIcon />
+                    </Button>
+                    <Button onClick={(e) => editBillForm(e, 'remove', index)}>
+                      <RemoveIcon />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+              <label>Total: </label>
+              <TextField
+                InputProps={{
+                  readOnly: true
+                }}
+                name="total"
+                id="total"
+                type="number"
+                value={formik.values.total}
+              />
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    )
+  } else {
+    return null
+  }
+}
+
+export default BillForm
